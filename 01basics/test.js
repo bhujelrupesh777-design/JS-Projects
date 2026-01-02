54beb54a0e0130d921e51418605d5ace54f97b34
bhujelrupesh777-design <bhujel.rupesh777@gmail.com>
2026-01-02 12:07:05 +0000

    variable investigation

--- 01basics/test.js
+++ 01basics/test.js
@@ -1,1 +1,7 @@
 console.log("hey it's woking");
+const accountId = 12345
+let accountName ="Rupesh"
+accountId2 = 3456
+let accountId3;
+
+console.table([accountId, accountName, accountId2, accountId3])
